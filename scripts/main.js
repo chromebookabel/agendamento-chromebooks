b0d8349c943a1ad3eaa67379dbc0723df0216d2e
Ana Clara <anaclara.gomes@lasalle.org.br>
2024-05-15 17:25:09 -0300

    Adicionando todos os arquivos do projeto

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+let btnCarrinho1 = document.querySelector('.carrinho1')
+let btnCarrinho2 = document.querySelector('.carrinho2')
+let btnCarrinho3 = document.querySelector('.carrinho3')
+
+// Função para verificar se é um dispositivo móvel
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+// Função para redirecionar para a página especificada
+function redirecionar(pagina) {
+    if(isMobile()) {
+        setTimeout(function() {
+            window.open(pagina, '_blank');
+        }, 1500)
+    }
+    else {
+        setTimeout(function() {
+            window.open(pagina, '_blank');
+        }, 800)
+    }
+}
